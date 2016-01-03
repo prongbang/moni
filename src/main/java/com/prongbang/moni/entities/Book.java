6cc163e3bf4b3331c703238f8156b203dbfcb663
prongbang <prongbang@gmail.com>
2016-01-03 21:46:20 +0700

    entities

--- src/main/java/com/prongbang/moni/entities/Book.java
+++ src/main/java/com/prongbang/moni/entities/Book.java
@@ -4,26 +4,17 @@ import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
+import javax.persistence.Id; 
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.validation.constraints.Size; 
 
 /**
  *
  * @author prongbang
  */
 @Entity
-@Table(name = "book", catalog = "spring", schema = "")
-@XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "Book.findAll", query = "SELECT b FROM Book b"),
-    @NamedQuery(name = "Book.findById", query = "SELECT b FROM Book b WHERE b.id = :id"),
-    @NamedQuery(name = "Book.findByName", query = "SELECT b FROM Book b WHERE b.name = :name"),
-    @NamedQuery(name = "Book.findByPrice", query = "SELECT b FROM Book b WHERE b.price = :price")})
+@Table(name = "book")
 public class Book implements Serializable {
 
     private static final long serialVersionUID = 1L;
